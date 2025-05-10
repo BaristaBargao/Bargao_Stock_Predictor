a3400c189df9c730fec163eef51a882a00222588
BaristaBargao <wuu00002@gmail.com>
2025-05-11 01:08:44 +0800

    更新 requirements.txt 和修正部署設定

--- app.py
+++ app.py
@@ -178,5 +178,8 @@ def history():
     return render_template('history.html', records=records)
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
